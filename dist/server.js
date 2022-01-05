6489c0f2810797880afc5d2265a3f01d9e2115d2
Chase Faggard <chasefaggard@Chases-MacBook-Pro.local>
2022-01-05 17:37:28 -0600

    Removing dist and updating .gitignore

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-const StockServer_1 = require("./StockServer");
-let app = new StockServer_1.StockServer().getApp();
-exports.app = app;
-//# sourceMappingURL=server.js.map
